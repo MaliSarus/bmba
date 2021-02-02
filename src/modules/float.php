c44fe8773ca02ee3a37d6f107178092a2bf3a7cd
MaliSarus <43217693+MaliSarus@users.noreply.github.com>
2021-02-02 13:05:05 +0300

    fixes header__logo

---
+++ src/modules/float.php
@@ -0,0 +1,131 @@
+<section class="float-section first-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="float-section__title">
+                    <h1>Флот</h1>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="float-section__block">
+                    <div class="list text float-section__list">
+                        <ul>
+                            <li><a href="#">
+                                    <div class="float-section__item">
+                                        <div class="float-section__image"><img
+                                                    src="http://bmba.sotbisite.beget.tech/upload/iblock/146/1465be6cd11887ee9f0bd83f93bfbd9e.jpg"
+                                                    alt="Кижуч"></div>
+                                        <div class="float-section__desc">
+                                            <h2></h2>
+                                            <ul>
+                                                <li>Мощность: 5/5</li>
+                                                <li>Тяга: офигенная</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </a></li>
+                            <li><a href="#">
+                                    <div class="float-section__item">
+                                        <div class="float-section__image"><img
+                                                    src="http://bmba.sotbisite.beget.tech/upload/iblock/d00/d00f05ff36b55a333baff0dfacb25d4d.jpg"
+                                                    alt="Черная барабулька"></div>
+                                        <div class="float-section__desc">
+                                            <h2></h2>
+                                            <ul>
+                                                <li>Мощность: 5/5</li>
+                                                <li>Тяга: офигенная</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </a></li>
+                            <li><a href="#">
+                                    <div class="float-section__item">
+                                        <div class="float-section__image"><img
+                                                    src="http://bmba.sotbisite.beget.tech/upload/iblock/fc4/fc43090259cae662be9ecb665477a390.jpg"
+                                                    alt="Навага"></div>
+                                        <div class="float-section__desc">
+                                            <h2></h2>
+                                            <ul>
+                                                <li>Мощность: 5/5</li>
+                                                <li>Тяга: офигенная</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </a></li>
+                            <li><a href="#">
+                                    <div class="float-section__item">
+                                        <div class="float-section__image"><img
+                                                    src="http://bmba.sotbisite.beget.tech/upload/iblock/7fc/7fc8bfad1e593fc0b1d0096f4fda9ba3.jpg"
+                                                    alt="Белуга"></div>
+                                        <div class="float-section__desc">
+                                            <h2></h2>
+                                            <ul>
+                                                <li>Мощность: 5/5</li>
+                                                <li>Тяга: офигенная</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </a></li>
+                            <li><a href="#">
+                                    <div class="float-section__item">
+                                        <div class="float-section__image"><img
+                                                    src="http://bmba.sotbisite.beget.tech/upload/iblock/eb5/eb5318249d948fe219b807dd2157bc60.jpg"
+                                                    alt="Таймень"></div>
+                                        <div class="float-section__desc">
+                                            <h2></h2>
+                                            <ul>
+                                                <li>Мощность: 5/5</li>
+                                                <li>Тяга: офигенная</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </a></li>
+                            <li><a href="#">
+                                    <div class="float-section__item">
+                                        <div class="float-section__image"><img
+                                                    src="http://bmba.sotbisite.beget.tech/upload/iblock/764/764fa6b5ec8c4ca64db953e7690a9b82.jpg"
+                                                    alt="Севрюга"></div>
+                                        <div class="float-section__desc">
+                                            <h2></h2>
+                                            <ul>
+                                                <li>Мощность: 5/5</li>
+                                                <li>Тяга: офигенная</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </a></li>
+                            <li><a href="#">
+                                    <div class="float-section__item">
+                                        <div class="float-section__image"><img
+                                                    src="http://bmba.sotbisite.beget.tech/upload/iblock/231/2312d217fcd9a92f143a907c028bfb87.jpg"
+                                                    alt="Феликс"></div>
+                                        <div class="float-section__desc">
+                                            <h2></h2>
+                                            <ul>
+                                                <li>Мощность: 5/5</li>
+                                                <li>Тяга: офигенная</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </a></li>
+                            <li><a href="#">
+                                    <div class="float-section__item">
+                                        <div class="float-section__image"><img
+                                                    src="http://bmba.sotbisite.beget.tech/upload/iblock/538/538789f50d32ce5aa49f992f08e05e2c.jpg"
+                                                    alt="Мартин"></div>
+                                        <div class="float-section__desc">
+                                            <h2></h2>
+                                            <ul>
+                                                <li>Мощность: 5/5</li>
+                                                <li>Тяга: офигенная</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
